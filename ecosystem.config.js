5431717e8180383e8eb337eb1372aa1c6b58f1e7
gwangrakhong <seeds.coffee@gmail.com>
2022-08-13 22:27:10 +0900

    Init Project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  apps: [
-        name: "MSSS BE",
-        script: "./dist/src/main.js",
-        max_memory_restart: "1G",
-        kill_timeout: 5000,
-        env: {
-            NODE_ENV: "dev",
-        },
-        env_staging:{
-            NODE_ENV:"staging"
-        },
-        env_production: {
-            NODE_ENV: "production",
-        }
-    }]
+  apps : [{
+    name: "app",
+    script: "./dist/src/main.js",
+    max_memory_restart: "3G",
+    kill_timeout: 5000,
+    env: {
+      NODE_ENV: "dev",
+    },
+    env_staging:{
+      NODE_ENV:"staging"
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
 }
