bf88f34512f0aa7041b0b15d0c731ce48c4f9d2a
gwangrakhong <seeds.coffee@gmail.com>
2023-01-15 19:23:02 +0900

    Init Project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from "@nestjs/common";
 import { AppService } from './app.service';
-
+import { MessagePattern } from '@nestjs/microservices';
+const logger = new Logger('AppController');
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'getMagazines' })
+  getHello() {
+    logger.log('getHello');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+const logger = new Logger('Main');
+const microserviceOptions = {
+  name: 'GREETING_SERVICE',
+  transport: Transport.REDIS,
+  options: {
+    host: 'www.devkids.co.kr',
+    port: 14408,
+    auth_pass: 'devnewmingcache',
+  },
+};
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+  await app.listen();
 }
 bootstrap();
